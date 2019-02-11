20be9a4c5293d60d7f67c5ff4055c26da3f73170
Gregory Niebanck <gniebanc@vt.edu>
2019-02-10 20:15:42 -0500

    skeleton

---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <router-link to="/" class="navbar-brand">Home Page</router-link>
+            </div>
+
+            
+        </div><!-- /.container-fluid -->
+    </nav>
+</template>
+
+<script>
+    export default {
+           
+    }
+</script>
---
+++ src/store/actions.js
@@ -0,0 +1,5 @@
+import Vue from 'vue';
+
+export const loadData = ({ commit }) => {
+    console.log(JSON.stringify(commit));
+};
